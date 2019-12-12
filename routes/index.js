d0a97191e03906f459fe03120128fd5bc6b0e8e7
robbiesanchez <rfsanchez004@gmail.com>
2019-12-12 12:25:00 -0500

    post request added

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,16 @@ router.get("/donate", async (req, res, next) => {
   }
 });
 
+router.post('/fund', (req, res, next) => {
+  const { user, title, description, amount  } = req.body;
+  const newFund = new Fund({ user, title, description, amount })
+  newFund.save()
+  .then((fund) => {
+    res.redirect('/fund/:id');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
 module.exports = router;
